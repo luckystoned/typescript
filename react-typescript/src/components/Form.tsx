01dff2ebde6a79c7d3a537a79f16fb37e7e77888
luckystoned <luckystoned.dev@gmail.com>
2022-05-15 01:19:07 -0300

    custom hook reducer

--- react-typescript/src/components/Form.tsx
+++ react-typescript/src/components/Form.tsx
@@ -1,52 +1,18 @@
-import { useState, useReducer } from "react";
+import useNewSubForm from '../hooks/useNewSubForm';
 import { Sub } from '../types';
 
-interface FormState {
-    inputValues: Sub
-}
+
 
 interface FormProps {
     onNewSub: (newSub: Sub) => void
 }
 
-const INITIAL_STATE = {
-    nick: "",
-    subsMonths: 0,
-    avatar: "",
-    description: ""
-}
-
-type FormReducerAction = {
-    type: "change_value"
-    payload: {
-        inputName: string
-        inputValue: string
-    }
-} | {
-    type: "clear"
-}
-
-const formReducer = (state: FormState["inputValues"], action: FormReducerAction) => {
-    switch (action.type) {
-        case "change_value":
-            const { inputName, inputValue} = action.payload;
-            return {
-                ...state,
-                [inputName]: inputValue
-            }
-        case "clear":
-            return INITIAL_STATE;
-        
-        default:
-            return state;
-    }
-}
 
 const Form = ({ onNewSub }: FormProps) => {
 
     //const [inputValues, setInputValues] = useState<FormState['inputValues']>(INITIAL_STATE);
 
-    const [inputValues, dispatch] = useReducer(formReducer, INITIAL_STATE)
+    const [inputValues, dispatch] = useNewSubForm()
     
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
