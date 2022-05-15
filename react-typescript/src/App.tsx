23bafc9a78d9001c1f8f2e021939c7eeecc46ed8
luckystoned <luckystoned.dev@gmail.com>
2022-05-15 01:55:07 -0300

    fetch to api and type response with functional programming

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -2,45 +2,58 @@ import { useEffect, useState, useRef } from 'react';
 import List from './components/List';
 import Form from './components/Form';
 
-import { Sub } from './types';
+import { Sub, SubsResponseFromApi } from './types';
 
 import './App.css';
 
 interface AppState {
   subs: Array<Sub>,
+  newSubsNumber: number
 }
-
-const INITIAL_STATE = [
-  {
-    nick: "lucas",
-    subsMonths: 3,
-    avatar: "https://avatars2.githubusercontent.com/u/100928?v=4",
-    description: "lucas is a frontend developer"
-  },
-  {
-    nick: "lucky",
-    subsMonths: 1,
-    avatar: "https://avatars2.githubusercontent.com/u/527123?&v=4"
-  }
-]
 
 function App() {
   
   const [subs, setSubs] = useState<AppState["subs"]>([]);
+  const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0);
   const divRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    setSubs(INITIAL_STATE);
+
+    // Typed fetch to get subs from API
+    const fetchSubs = (): Promise<SubsResponseFromApi> => {
+      return fetch('https://mocki.io/v1/15756bc0-6248-4e68-8c18-2586012da502')
+        .then(res => res.json())
+    }
+    // Adding desired shape to the response
+    const mapFromApiToSubs = (apiResponse: SubsResponseFromApi): Array<Sub> => {
+      return apiResponse.map(subFromApi => {
+        const { nick, subsMonths, avatar, description } = subFromApi;
+
+        return {
+          nick,
+          subsMonths,
+          avatar,
+          description
+        }
+      })
+    }
+
+    fetchSubs()
+      //Funcional Programming
+      .then(mapFromApiToSubs)
+      .then(setSubs)
   }, []);
 
   const handleNewSub = (newSub: Sub) => {
     setSubs(subs => [...subs, newSub]);
+    setNewSubsNumber(n => n + 1);
   }
 
   return (
     <div className="App" ref={divRef}>
       <h1>Luckys Subs!</h1>
       <List subs={subs} />
+      New Subs: {newSubsNumber}
       <Form onNewSub={handleNewSub} />
     </div>
   );
--- react-typescript/src/types.d.ts
+++ react-typescript/src/types.d.ts
@@ -1,3 +1,4 @@
+import { type } from "os"
 
 export interface Sub {
     nick: string,
@@ -5,3 +6,10 @@ export interface Sub {
     avatar: string,
     description?: string
 }
+
+export type SubsResponseFromApi = Array<{
+    nick: string,
+    subsMonths: number,
+    avatar: string,
+    description?: string
+}>
